af8387f936b90afc701a9b4feb1d0b3b9e4a77b9
Vladimir <39743965+vokzvokz@users.noreply.github.com>
2020-01-15 03:07:56 +0300

    Убрал захардкоженные штуки, добавил приоритеты

--- QueuingSystem.cpp
+++ QueuingSystem.cpp
@@ -1,10 +1,5 @@
-//
-// Created by Vladimir on 11.01.2020.
-//
-#define QUEUE_LIMIT 36
 #define BUISY      1
 #define IDLE       0
-#define N_SERVERS  2
 
 #include "QueuingSystem.h"
 #include <cstdio>
@@ -13,43 +8,48 @@
 #include <cstdlib>
 #include <ctime>
 
-QueuingSystem::QueuingSystem(double mean_service, double mean_interarrival, int num_requests_required,
-                               bool get_events_log, bool get_debug_log) {
+QueuingSystem::QueuingSystem(double mean_service, double mean_interarrival, double time_limit, int queue_limit, int n_servers,
+                             bool get_events_log, bool get_debug_log) {
     srand(time(NULL));
-
-    this->num_events = N_SERVERS+1;
+    this->n_servers = n_servers;
+    this->server_status = new int[n_servers];
+    this->time_next_event = new double[n_servers+2];
+    this->n_priorities = 6;
+    this->queue_limit = queue_limit;
+    this->num_events = n_servers+1;
     this->mean_service = mean_service;
     this->mean_interarrival = mean_interarrival;
-    this->num_requests_required = num_requests_required;
+    this->time_limit = time_limit;
     this->get_events_log = get_events_log;
     this->get_debug_log = get_debug_log;
 }
 
 void QueuingSystem::run() {
     system_time = 0;
+
     report_string = "Система массового обслуживания\n\nСреднее время между поступлениями " +
-                    to_string(mean_interarrival) + " мин\n\nСреднее время обслуживания " +
-                    to_string(mean_service)+" мин\n\nЧисло требований " + to_string(num_requests_required);
+                    to_string(mean_interarrival) + " c\n\nСреднее время обслуживания " +
+                    to_string(mean_service);
     // all servers are free
-    for (int &status : server_status) {
-        status = IDLE;
+    for (int i = 0; i<n_servers; i++) {
+        server_status[i] = IDLE;
     }
     total_waiting_in_queue = 0;
     total_being_in_system = 0;
-    num_in_q = 0;
     time_last_event = 0;
 
+    n_requests_rejected = 0;
     n_requests_served = 0;
     n_requests_arrived = 0;
     area_num_in_q = 0;
 
     time_next_event[1] = system_time + expon(mean_interarrival);
-    for(int i = 2; i<N_SERVERS+2; i++){
+    for(int i = 2; i<n_servers+2; i++){
         time_next_event[i] = 1.0e+30;
     }
 
     if(get_events_log) report_string+="----------------------------------\n\n";
-    while (n_requests_served<num_requests_required){
+    while (system_time<time_limit){
         timing();
         update_time_avg_stats();
         if(next_event_type == 1){
@@ -63,6 +63,19 @@ void QueuingSystem::run() {
 
 double QueuingSystem::expon(double mean) {
     return -mean * log((double) rand()/RAND_MAX);
+}
+
+int QueuingSystem::generate_priority() {
+    double borders[] = {0.5, 0.7, 0.85, 0.95, 0.98};
+    double X=((double)rand()/(double)RAND_MAX);
+    for(int i = 0; i<n_priorities-1; i++){
+        if(X<borders[i]){
+            //if(get_debug_log) report_string+="priority: "+to_string(n_priorities-i)+"\n\n";
+            //std::cout<<"X: "<<X<<" border: "<<borders[i]<<" priority: "+to_string(n_priorities-i)+"\n";
+            return n_priorities-i;
+        }
+    }
+    return 1;
 }
 
 void QueuingSystem::timing() {
@@ -94,7 +107,7 @@ void QueuingSystem::arrive() {
     time_next_event[1] = system_time + expon(mean_interarrival);
     n_requests_arrived++;
     if(get_events_log) report_string+="Прибыл запрос в момент времени "+to_string(system_time)+"\n\n";
-    for(int i = 0; i<N_SERVERS; i++){
+    for(int i = 0; i<n_servers; i++){
         if(server_status[i] == IDLE){
             //delay = 0;
             n_requests_served++;
@@ -103,41 +116,39 @@ void QueuingSystem::arrive() {
             total_being_in_system += time_next_event[i+2]-system_time;
             served = true;
             if(get_events_log)  report_string+="Запрос обработан сервером "+to_string(i)+" в момент времени " +
-                    to_string(system_time) + " с задержкой " + to_string(delay) + "\n\n";
+                                               to_string(system_time) + " с задержкой " + to_string(delay) + "\n\n";
             break;
         }
     }
     if(!served){
-        num_in_q++;
-        if(num_in_q > QUEUE_LIMIT){
+        if(requests_queue.size() > queue_limit){
             report_string+="Очередь переполнена в момент времени "+to_string(system_time)+"\n\n";
-            exit(2);
+            n_requests_rejected++;
+            //exit(2);
         }
-        time_arrival[num_in_q] = system_time;
+        requests_queue.push(make_pair(generate_priority(), system_time));
     }
 }
 
 void QueuingSystem::depart(int server_index) {
     double delay;
     if(get_debug_log) report_string+="depart  server_index = "+to_string(server_index) +
-            "  system time = " + to_string(system_time) + "\n\n";
-    if(num_in_q == 0){
+                                     "  system time = " + to_string(system_time) + "\n\n";
+    if(requests_queue.empty()){
         server_status[server_index] = IDLE;
         time_next_event[server_index+2] = 1.0e+30;
     } else {
-        num_in_q--;
+        delay = system_time - requests_queue.top().second;
+        requests_queue.pop();
         time_next_event[server_index+2] = system_time + expon(mean_service);
-        delay = system_time - time_arrival[1];
+
         total_waiting_in_queue += delay;
         total_being_in_system += (delay + (time_next_event[server_index+2]-system_time));
         n_requests_served++;
 
         if(get_events_log){
             report_string+="Запрос обработан сервером " + to_string(server_index) +
-                    " в момент времени " + to_string(system_time) + " с задержкой " + to_string(delay) + "\n\n";
-        }
-        for(int i = 1; i <= num_in_q; i++){
-            time_arrival[i] = time_arrival[i+1];
+                           " в момент времени " + to_string(system_time) + " с задержкой " + to_string(delay) + "\n\n";
         }
     }
 }
@@ -145,25 +156,29 @@ void QueuingSystem::depart(int server_index) {
 void QueuingSystem::update_time_avg_stats() {
     double time_since_last_event = system_time - time_last_event;
     time_last_event = system_time;
-    area_num_in_q += num_in_q*time_since_last_event;
+    area_num_in_q += requests_queue.size()*time_since_last_event;
 }
 
 string QueuingSystem::report() {
     string suffix = "";
-    suffix+="\n\nКоэфициент использования "+to_string((total_being_in_system-total_waiting_in_queue)/(system_time*N_SERVERS));
+    suffix+="\n\nКоэфициент использования "+to_string((total_being_in_system-total_waiting_in_queue)/(system_time*n_servers));
     suffix+="\n\nСреднее время ожидания в очереди " + to_string(total_waiting_in_queue / n_requests_served) + " с";
     suffix+="\n\nСреднее время пребывания в системе " + to_string(total_being_in_system / n_requests_served) + " с";
     suffix+="\n\nСреднее число требований в очереди " + to_string(total_waiting_in_queue / system_time);
     suffix+="\n\nСреднее число требований в системе " + to_string(total_being_in_system / system_time);
-    suffix+="\n\nАбсолютная пропускная способность " + to_string(n_requests_served/system_time); //среднее число заявок, которое может обслужить система за единицу времени.
-    suffix+="\n\nОтносительная пропускная способность " + to_string(n_requests_served/n_requests_arrived); //отношение среднего числа заявок, обслуживаемых системой в единицу времени, к среднему числу поступающих за это время заявок
 
+    //среднее число заявок, которое может обслужить система за единицу времени.
+    suffix+="\n\nАбсолютная пропускная способность " + to_string(n_requests_served/system_time);
+    //отношение среднего числа заявок, обслуживаемых системой в единицу времени, к среднему числу поступающих за это время заявок
+    suffix+="\n\nОтносительная пропускная способность " + to_string(n_requests_served/n_requests_arrived);
+
+    suffix+="\n\nЧисло необслуженных требований " + to_string(n_requests_rejected);
     suffix+="\n\nЧисло обслуженных требований " + to_string(n_requests_served);
     suffix+="\n\nВремя завершения моделирования "+ to_string(system_time) +" с";
     return report_string+suffix;
 }
 
 QueuingSystem::~QueuingSystem() {
-
+    delete [] time_next_event;
+    delete [] server_status;
 }
-
--- QueuingSystem.h
+++ QueuingSystem.h
@@ -1,32 +1,46 @@
-//
-// Created by Vladimir on 11.01.2020.
-//
-
 #ifndef SMO_ALPHA_QUEUING_SYSTEM_H
 #define SMO_ALPHA_QUEUING_SYSTEM_H
 
 #include <string>
+#include <functional>
+#include <queue>
+#include <vector>
+#include <iostream>
 
-#define QUEUE_LIMIT 36
 #define BUISY      1
 #define IDLE       0
-#define N_SERVERS  2
+//#define N_SERVERS  2
 
 using namespace std;
+
+/* TODO
+ * Изменить критерий останова на время   +
+ * Реализовать приоритет                 +
+ * Выбор размера накопителя              +
+ * Выбор количества устройств            +
+ * Матожидание, дисперсия, дов интервал  -
+ *
+*/
 
 class QueuingSystem {
 private:
 
 
-    int next_event_type, n_requests_served, n_requests_arrived, num_requests_required, num_events;
-    int num_in_q, server_status[N_SERVERS];
+    int next_event_type, n_requests_served, n_requests_arrived, n_priorities, queue_limit, num_events;
+    int n_requests_rejected, n_servers;
+    //int server_status[N_SERVERS];
+    int *server_status;
 
-    double area_num_in_q, area_server_status, mean_interarrival, mean_service;
-    double system_time, time_arrival[QUEUE_LIMIT+1], time_last_event, time_next_event[N_SERVERS+2];
+    double area_num_in_q, mean_service;
+    double system_time, time_limit, time_last_event;//, time_next_event[N_SERVERS+2];
+    double *time_next_event;
     double total_waiting_in_queue, total_being_in_system;
 
     bool get_events_log, get_debug_log;
     string report_string;
+
+    priority_queue<pair<int, double>> requests_queue;
+
     //void initialize();
     void timing();
     void arrive();
@@ -34,12 +48,14 @@ private:
 
     void update_time_avg_stats();
     double expon(double mean);
+    int generate_priority();
+    string get_stats(string spacer = " ");
 
 public:
     string report();
     void run();
-    QueuingSystem(double mean_service, double mean_interarrival, int num_requests_required,
-            bool get_events_log = false, bool get_debug_log = false);
+    QueuingSystem(double mean_service, double mean_interarrival, double  time_limit, int queue_limit = 36,
+                  int n_servers = 2, bool get_events_log = false, bool get_debug_log = false);
     ~QueuingSystem();
 };
 
